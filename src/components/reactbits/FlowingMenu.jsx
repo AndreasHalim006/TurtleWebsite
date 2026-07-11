7e0b1e37502b2dc4e635c08e09c2c26fc084b711
AndreasHalim006 <andreashal006@gmail.com>
2026-07-11 21:43:19 +0300

    fix: replace border-top with inset box-shadow in FlowingMenu to prevent sub-pixel layout clipping

--- src/components/reactbits/FlowingMenu.jsx
+++ src/components/reactbits/FlowingMenu.jsx
@@ -121,7 +121,7 @@ function MenuItem({ id, id: itemID, link, text, image, speed, textColor, marquee
     <div
       className="flex-1 relative overflow-hidden flex items-center justify-center"
       ref={itemRef}
-      style={{ borderTop: isFirst ? 'none' : `1px solid ${borderColor}` }}
+      style={{ boxShadow: isFirst ? 'none' : `inset 0  1px 0 0 ${borderColor}` }}
     >
       <a
         className="flex items-center justify-center w-full h-full relative cursor-pointer uppercase no-underline font-semibold text-[4vh] z-10"
